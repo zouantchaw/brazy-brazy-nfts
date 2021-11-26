8aece8ee3ebe8bb860b844ebc7b097c505a76003
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-26 12:58:08 -0500

    import openzepplin contracts

--- contracts/BrazyNFT.sol
+++ contracts/BrazyNFT.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.0;
 
+// Import OpenZepplin contracts
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
 contract BrazyNFT {
