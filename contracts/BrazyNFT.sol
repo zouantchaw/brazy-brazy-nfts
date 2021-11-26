4d934674ab9e83b77664a1e790bc2db2133ee79c
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-11-26 13:53:55 -0500

    implement create NFT function

--- contracts/BrazyNFT.sol
+++ contracts/BrazyNFT.sol
@@ -7,8 +7,30 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract BrazyNFT {
-    constructor() {
+// Inherit contract imported from OpenZepplin,
+// giving BrazyNFT inherited contract methods
+contract BrazyNFT is ERC721URIStorage {
+    // Counter from openzepplin to keep track of tokenIds
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // Pass in NFTs token and symbol
+    constructor() ERC721("CircleNFT", "CIRCLE") {
         console.log("Hello from smart contact");
+    }
+
+    // When invoked, creates NFT for user
+    function makeABrazyNFT() public {
+        // Get current tokenId
+        uint256 newItemId = _tokenIds.current();
+
+        // Mint NFT to sender
+        _safeMint(msg.sender, newItemId);
+
+        // Set NFT data
+        _setTokenURI(newItemId, "Hello Hello");
+
+        // Increment counter to allow next NFT to be minted
+        _tokenIds.increment();
     }
 }
