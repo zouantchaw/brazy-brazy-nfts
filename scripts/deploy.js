f39e81db1650f8d4b202e8c668a926f0b24a3afe
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-01 10:41:35 -0500

    deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory('BrazyNFT');
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contact deployed to:", nftContract.address);
+
+  // Call contract function
+  let txn = await nftContract.makeABrazyNFT()
+  // Wait for it to be mined
+  await txn.wait()
+  console.log("Minted NFT #1")
+
+  // Mint another NFT
+  txn = await nftContract.makeABrazyNFT()
+  // Wait for it to be mined
+  await txn.wait()
+  console.log("Minted NFT #2")
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
